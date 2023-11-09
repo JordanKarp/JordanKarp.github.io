8f8290d38b35227f0927e2288e743494e6bf9423
Jordan Karp <Jordanmatthew.karp@gmail.com>
2023-11-09 11:54:53 -0800

    Particle attempt

---
+++ particlesScript.js
@@ -0,0 +1,110 @@
+particlesJS('particles', {
+    "particles": {
+      "number": {
+        "value": 303,
+        "density": {
+          "enable": true,
+          "value_area": 400.8530152163807
+        }
+      },
+      "color": {
+        "value": "#ffffff"
+      },
+      "shape": {
+        "type": "circle",
+        "stroke": {
+          "width": 0,
+          "color": "#000000"
+        },
+        "polygon": {
+          "nb_sides": 5
+        },
+        "image": {
+          "src": "img/github.svg",
+          "width": 100,
+          "height": 100
+        }
+      },
+      "opacity": {
+        "value": 0.7135183670851577,
+        "random": true,
+        "anim": {
+          "enable": false,
+          "speed": 1,
+          "opacity_min": 0.1,
+          "sync": false
+        }
+      },
+      "size": {
+        "value": 3,
+        "random": true,
+        "anim": {
+          "enable": false,
+          "speed": 40,
+          "size_min": 0.1,
+          "sync": false
+        }
+      },
+      "line_linked": {
+        "enable": false,
+        "distance": 150,
+        "color": "#ffffff",
+        "opacity": 0.4,
+        "width": 1
+      },
+      "move": {
+        "enable": true,
+        "speed": 0.9,
+        "direction": "bottom",
+        "random": true,
+        "straight": true,
+        "out_mode": "out",
+        "bounce": false,
+        "attract": {
+          "enable": true,
+          "rotateX": 7055.0130678083,
+          "rotateY": 8177.401510414166
+        }
+      }
+    },
+    "interactivity": {
+      "detect_on": "window",
+      "events": {
+        "onhover": {
+          "enable": true,
+          "mode": "repulse"
+        },
+        "onclick": {
+          "enable": false,
+          "mode": "push"
+        },
+        "resize": true
+      },
+      "modes": {
+        "grab": {
+          "distance": 400,
+          "line_linked": {
+            "opacity": 0
+          }
+        },
+        "bubble": {
+          "distance": 121.80465781011475,
+          "size": 4.060155260337158,
+          "duration": 0,
+          "opacity": 0.8363919836294545,
+          "speed": 3
+        },
+        "repulse": {
+          "distance": 40.60155260337158,
+          "duration": 0.4
+        },
+        "push": {
+          "particles_nb": 4
+        },
+        "remove": {
+          "particles_nb": 2
+        }
+      }
+    },
+    "retina_detect": true
+  })
